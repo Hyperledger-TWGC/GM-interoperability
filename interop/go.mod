4c9f9bcb7c398e812e1c71e0f2f7fd731e32be7b
SamYuan1990 <yy19902439@126.com>
2020-10-18 21:33:07 +0800

    decouple steps and making parallel jobs
    Signed-off-by: SamYuan1990 <yy19902439@126.com>

--- interop/go.mod
+++ interop/go.mod
@@ -3,7 +3,7 @@ module github.com/Hyperledger-TWGC/fabric-gm-plugins/interop
 go 1.14
 
 require (
-	github.com/Hyperledger-TWGC/ccs-gm v0.0.0-20200917142018-eeebcfc2577c
-	github.com/Hyperledger-TWGC/pku-gm v0.0.0-20200831163314-046f8c468eba
+	github.com/Hyperledger-TWGC/ccs-gm v0.1.2-0.20201231014652-bf5333c7512d
+	github.com/Hyperledger-TWGC/pku-gm v1.0.1
 	github.com/Hyperledger-TWGC/tjfoc-gm v1.3.3
 )
--- interop/util_test.go
+++ interop/util_test.go
@@ -1,6 +1,7 @@
 package interop
 
 import (
+	"fmt"
 	"io/ioutil"
 	"os"
 	"testing"
@@ -8,11 +9,16 @@ import (
 
 func Fatal(err error, t *testing.T) {
 	if err != nil {
+		fmt.Println("detail error msg")
+		fmt.Println(err)
 		t.Fatal(err)
 	}
 }
 func ReadFile(filename string, t *testing.T) []byte {
 	content, err := ioutil.ReadFile(filename)
+	fmt.Println(filename)
+	fmt.Println("File content")
+	fmt.Println(string(content))
 	Fatal(err, t)
 	return content
 }
