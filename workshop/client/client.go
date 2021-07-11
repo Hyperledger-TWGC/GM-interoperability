e02cc8df9bfbea3eba25166d348b4eddc706a471
Sam Yuan <yy19902439@126.com>
2021-07-11 22:18:56 +0800

    impl 1st version for server verify
    Signed-off-by: Sam Yuan <yy19902439@126.com>

--- workshop/client/client.go
+++ workshop/client/client.go
@@ -18,4 +18,12 @@ func main() {
 		decrypted, _ := priv.Decrypt(test)
 		fmt.Println(string(decrypted))
 	}
+	if os.Args[1] == "sign" {
+		fmt.Println("sign")
+		priv, _ := workshop.LoadFromPriPem(priFile)
+		var msg = []byte("2021-07-03 13:44:10")
+		sign, _ := workshop.DegistAndSign(msg, priv)
+		encodedStr := hex.EncodeToString(sign)
+		fmt.Println(string(encodedStr))
+	}
 }
--- workshop/server/server.go
+++ workshop/server/server.go
@@ -5,6 +5,7 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"strconv"
 
 	"github.com/Hyperledger-TWGC/fabric-gm-plugins/workshop"
 	restful "github.com/emicklei/go-restful/v3"
@@ -14,7 +15,7 @@ var pubFile = "pub.pem"
 
 func main() {
 	ws := new(restful.WebService)
-	ws.Route(ws.POST("/verify").To(verify))
+	ws.Route(ws.GET("/verify").To(verify))
 	ws.Route(ws.GET("/encrypt").To(encrypt))
 	restful.Add(ws)
 	log.Fatal(http.ListenAndServe(":8080", nil))
@@ -25,10 +26,11 @@ func verify(req *restful.Request, resp *restful.Response) {
 	Key, _ = workshop.LoadFromPubPem(pubFile)
 	var msg = []byte("2021-07-03 13:44:10")
 	//todo get sign from request
-	data := workshop.DegistAndVerify(msg, nil, Key)
+	dummy, _ := hex.DecodeString("304502201fdfdeaee05eb78013adf283f65de61d50adf7b6a792d41c994ed4a36775355b022100d6873ec4c6ebcf5ac97b88990ba1e0a9abe4e06f37e0cd05ef07849f7d24c519")
+	data := workshop.DegistAndVerify(msg, dummy, Key)
 	log.Println(data)
 	//todo string data is not human-readable
-	io.WriteString(resp, "data")
+	io.WriteString(resp, strconv.FormatBool(data))
 }
 
 func encrypt(req *restful.Request, resp *restful.Response) {
